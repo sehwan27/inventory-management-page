49a1d4f2d22ae3f374c22db38e7340c58e02f6e6
Seh Wan Loong <sehwan1997@hotmail.com>
2023-12-15 16:52:23 +0800

    navigation

--- inventory-management-page/src/components/TopNav.vue
+++ inventory-management-page/src/components/TopNav.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
     <div class="title-container">
-      <span style="font-family: sans-serif;">
+
+      <span style="font-family: sans-serif">
         <span class="mb-0 me-2 inventory-text">INVENTORY</span>
         <span class="management-text">MANAGEMENT</span>
-        </span
-      >
+      </span>
+    </div>
+
+    <div class="navbar" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link to="/category" class="nav-link">Category</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/product" class="nav-link">Product</router-link>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -18,14 +29,32 @@ export default {};
 @import "@/style/variables.scss";
 @import "@/style/navigation.scss";
 
+.navbar {
+  background-color: $primary-1;
+}
+
+.nav-link, .nav-item {
+  width: fit-content;
+}
+
+.navbar-nav{
+  flex-direction: row;
+}
+
+.nav-item {
+  padding: 5px 10px;
+}
+
+
+
 .inventory-text {
-    color: $primary-1;
-    font-weight: 900;
-    font-size: 20px;
+  color: $primary-1;
+  font-weight: 900;
+  font-size: 20px;
 }
 
 .management-text {
-    color: $primary-2;
-    font-weight: bold;
+  color: $primary-2;
+  font-weight: bold;
 }
 </style>
